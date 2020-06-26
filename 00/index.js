9c33bd7ac4f6292a625ae023416cf19242cf7606
cammac <camm@rocktechcomputer.com>
2020-06-26 15:36:24 -0700

    completed 00 and 01 exercise

--- 00/index.js
+++ 00/index.js
@@ -9,9 +9,43 @@ function output(object) {
 console.log('Welcome to the XState workshop!');
 
 const user = {
-  name: 'David Khourshid',
-  company: 'Microsoft',
-  interests: ['piano', 'state machines'],
+  name: 'Cam MacDonald',
+  company: 'Rock Tech Computer Services, Inc',
+  interests: ['golf', 'cycling', 'solving', 'coding'],
 };
 
 output(user);
+
+const machine = {
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        FETCH: 'pending',
+      }
+    },
+    pending: {
+      on: {
+        RESOLVE: 'resolved',
+        REJECT: 'rejected',
+      }
+    },
+    resolved: {},
+    rejected: {},
+  }
+}
+
+const transition = (state, event) => {
+  return machine.states[state]?.on?.[event] || state;
+};
+
+// idle
+let currentState = machine.initial;
+
+const send = (event) => {
+  const nextState = transition(currentState, event);
+
+  currentState = nextState;
+}
+
+window.send = send;
